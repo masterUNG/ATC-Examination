6e85a6ede1450f97ced40bed70c7d7dfc719546a
masterUNG <phrombutr@gmail.com>
2016-03-22 09:41:30 +0700

    Start

---
+++ app/src/main/java/appewtc/masterung/ungrestaurant/MainActivity.java
@@ -0,0 +1,67 @@
+package appewtc.masterung.ungrestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private String userString, passwordString;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    }   // Main Method
+
+    private void buttonController() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Receive Value From Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    MyAlertDialog myAlertDialog = new MyAlertDialog();
+                    myAlertDialog.myDialog(MainActivity.this,
+                            "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+
+                } else {
+                    //No Space
+
+                }
+
+            }   // onClick
+        });
+
+    }   // buttonController
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+        loginButton = (Button) findViewById(R.id.button);
+
+    }
+
+}   // Main Class นี่คือ คลาสหลัก
---
+++ app/src/main/java/appewtc/masterung/ungrestaurant/MyAlertDialog.java
@@ -0,0 +1,31 @@
+package appewtc.masterung.ungrestaurant;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 3/21/16 AD.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.icon_question);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton("จร้า", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+}   // Main Class
